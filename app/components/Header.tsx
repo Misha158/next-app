f57d64ae6df529c5201a443a451f5a21ed03f4db
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-09-13 01:28:12 +0300

    Prettier problem

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 import Link from "next/link";
 
 export const Header = () => {
-    return (
-        <div>
-            <Link href={"/"}>Home</Link>
-            <Link href={"/about"}>about</Link>
-            <Link href={"/page-2"}>Page 2</Link>
-
-            </div>
-    );
+  return (
+    <div>
+      <Link href={"/"}>Home</Link>
+      <Link href={"/about"}>about</Link>
+      <Link href={"/page-2"}>Page 2</Link>
+    </div>
+  );
 };
