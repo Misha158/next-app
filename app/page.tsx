63de8d441e3bda061f83245089fce8d3ce5fb0c8
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-09-13 11:10:20 +0300

    Tried fetch data on server

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-export default function Home() {
+const getTodos = async () => {
+  const todos = await fetch("https://jsonplaceholder.typicode.com/todos").then(
+    (response) => response.json()
+  );
+
+  return todos;
+};
+
+export default async function Home() {
+  const todos = await getTodos();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  )
+    <main className="flex min-h-screen flex-col items-start justify-between p-24">
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <div>
+            {todo.id}. {todo.title}
+          </div>
+        </div>
+      ))}
+    </main>
+  );
 }
